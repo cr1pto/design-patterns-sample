fc1f23563edcc4de5d1482d4fe7e10ccba684667
cr1pto <20729543+cr1pto@users.noreply.github.com>
2025-05-27 22:30:52 -0700

    Add factory method sample

--- csharp/DesignPatterns.Lib/Patterns/Basic/Factory.cs
+++ csharp/DesignPatterns.Lib/Patterns/Basic/Factory.cs
@@ -16,6 +16,79 @@ public abstract class Factory : IDesignPattern
     public abstract string GetPatternOutput();
 }
 
+//creator class
+public abstract class Dashboard
+{
+    //factory method: CreateDiagram
+    public abstract Diagram CreateDiagram();
+
+    //Share is a functionality to share the dashboard with another person.
+    public void Share()
+    {
+        var diagram = CreateDiagram();
+
+        var relevantData = diagram.GetDiagramData();
+
+        //do something with the relevant        
+    }
+}
+
+public class EmailDashboard : Dashboard
+{
+    public override Diagram CreateDiagram()
+    {
+        return new EmailsDiagram();
+    }
+}
+
+public class ApplicationAlertsDashboard : Dashboard
+{
+    public override Diagram CreateDiagram()
+    {
+        return new ErrorDiagram();
+    }
+}
+
+public class LogsDashboard : Dashboard
+{
+    public override Diagram CreateDiagram()
+    {
+        return new LogsDiagram();
+    }
+}
+
+public interface Diagram
+{
+    object GetDiagramData();
+}
+
+public class LogsDiagram : Diagram
+{
+    public object GetDiagramData()
+    {
+        {
+            return "[Some XML/JSON/etc. with relevant data]";
+        }
+    }
+}
+
+public class ErrorDiagram : Diagram
+{
+    public object GetDiagramData()
+    {
+        return "Some information about errors here";
+    }
+}
+
+public class EmailsDiagram : Diagram
+{
+    public object GetDiagramData()
+    {
+        return "Some diagram showing a chart of emails sent through a given period";
+    }
+}
+
+// OOP Factory
 public class ShapeFactory : Factory
 {
     public override string GetPatternOutput()
@@ -23,6 +96,7 @@ public class ShapeFactory : Factory
         return GetShape(nameof(Rectangle)).GetType().FullName;
     }
 
+    //sample factory method to get a shape
     public IShape GetShape(string shapeType)
     {
         if (shapeType == null)
